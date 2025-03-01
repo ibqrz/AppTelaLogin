c5b5c383fee7d764bb5ad0db42cc053be5a203ba
ibqrz <isabelqa77@gmail.com>
2025-03-01 17:10:52 -0300

    Adicionar arquivos de projeto.

---
+++ AppTelaLogin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppTelaLogin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppTelaLogin/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace AppTelaLogin
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
