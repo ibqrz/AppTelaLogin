2ca7eb39cdf12b5cbd54ed0b5f05620639b421b2
ibqrz <isabelqa77@gmail.com>
2025-03-05 16:01:25 -0300

    teste2

---
+++ AppCalBasica/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppCalBasica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppCalBasica/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppCalBasica
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
